2e4eec43a8dedc685288ed63d0c37bdc51021833
Terence Cole <trcexcel@yahoo.com>
2020-04-22 18:43:59 -0500

    Added CSS file and added notes to JS file

---
+++ etch-a-sketch.js
@@ -0,0 +1,11 @@
+// Select the elements on the page - canvas, shake button
+
+// Set up the canvas for drawing
+
+// Write a draw function
+
+// Write a handler for keys
+
+// Clear or shake function
+
+// Listen for arrow keys
