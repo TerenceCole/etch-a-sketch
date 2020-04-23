95b26b6dcba03121ce1669d8c525efcbc6b8ee30
Terence Cole <trcexcel@yahoo.com>
2020-04-23 11:41:24 -0500

    Setting up canvas for JS

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,6 +1,17 @@
 // Select the elements on the page - canvas, shake button
 
+const canvas = document.querySelector('#etch-a-sketch');
+const ctx = canvas.getContext('2d');
+const shakebutton = document.querySelector('.shake');
+
 // Set up the canvas for drawing
+
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+ctx.lineWidth = 10;
+
+ctx.beginPath(); // start the drawing
+ctx.moveTo(200, 200);
 
 // Write a draw function
 
